be67905320439227502571d19f2dee0624af65f6
up1 <somkiat.p@gmail.com>
2022-04-25 12:49:51 +0700

    Working with application metric

---
+++ service3.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 3002
+
+const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
+const { MeterProvider }  = require('@opentelemetry/sdk-metrics-base');
+
+// Add your port and startServer to the Prometheus options
+const options = {port: 9464, startServer: true};
+const exporter = new PrometheusExporter(options);
+
+// Register the exporter
+const meter = new MeterProvider({
+  exporter,
+  interval: 1000,
+}).getMeter('demo-prometheus');
+
+// Now, start recording data
+const counter = meter.createCounter('count_movies', {
+  description: 'Count movies api'
+});
+
+
+app.get('/movies', async function (req, res) {
+
+  counter.add(1, { service_name: "service_3" });
+
+  res.type('json')
+  var delay = Math.floor( ( Math.random() * 2000 ) + 100);
+
+  setTimeout((() => {
+    res.send(({movies: [
+      { name: 'Jaws', genre: 'Thriller'}, 
+      { name: 'Annie', genre: 'Family'},
+      { name: 'Jurassic Park', genre: 'Action'},
+    ]}))
+  }), delay)
+
+})
+
+app.listen( port, () => { console.log(`Listening at http://localhost:${port}`)})
